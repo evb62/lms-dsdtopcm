29d4be20110d6fa6ed311da0878bcae913599636
Elon Brisola <84412778+evb62@users.noreply.github.com>
2026-08-22 12:05:56 -0300

    Update Log.pm

--- DSDTranscode/Log.pm
+++ DSDTranscode/Log.pm
@@ -1,5 +1,9 @@
 package Plugins::DSDTranscode::Log;
 
+# Central log category for the plugin. addLogCategory() registers the
+# category with LMS so it can be tuned under
+# Settings -> Advanced -> Logging; the description is a strings.txt key.
+
 use strict;
 use warnings;
 
@@ -8,14 +12,14 @@ use Slim::Utils::Log;
 my $log;
 
 sub get {
-    unless ($log) {
-        $log = Slim::Utils::Log->addLogCategory({
-            'category'     => 'plugin.dsdtranscode',
-            'defaultLevel' => 'WARN',
-            'description'  => 'PLUGIN_DSDTRANSCODE',
-        });
-    }
-    return $log;
+	unless ($log) {
+		$log = Slim::Utils::Log->addLogCategory({
+			'category'     => 'plugin.dsdtranscode',
+			'defaultLevel' => 'WARN',
+			'description'  => 'PLUGIN_DSDTRANSCODE',
+		});
+	}
+	return $log;
 }
 
 1;
